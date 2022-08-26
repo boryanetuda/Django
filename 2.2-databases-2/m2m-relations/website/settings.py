60c99fd43b7070b48582daddab185e9321498506
boryanetuda <boriig97@icloud.com>
2022-08-26 15:36:25 +0300

    Files changed

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_m2m_relations',
+        'NAME': 'hw1',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
